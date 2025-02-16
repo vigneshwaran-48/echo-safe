51e7813da4f826d567adb4b3fead3ca8e96a4420
vigneshwaran48 <p3487260@gmail.com>
2025-02-16 11:48:11 +0530

    Tailwindcss config changes due to v4

--- internal/templates/index_templ.go
+++ internal/templates/index_templ.go
@@ -29,7 +29,7 @@ func Index() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"bg-primary-600 p-4\">Welcome to Echo Safe</div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div class=\"bg-primary-500 bg-green-400 p-4\">Welcome to Echo Safe</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.templ"],
+  content: ["internal/templates/*.templ", "internal/templates/*.go"],
   theme: {
     container: {
       center: true,
