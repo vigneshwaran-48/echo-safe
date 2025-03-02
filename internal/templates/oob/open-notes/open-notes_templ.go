52d64dc97d63115a137b118894b8ab74937d8e75
vigneshwaran48 <p3487260@gmail.com>
2025-03-02 11:49:36 +0530

    Open notes bar changes

--- internal/templates/oob/open-notes/open-notes_templ.go
+++ internal/templates/oob/open-notes/open-notes_templ.go
@@ -32,7 +32,7 @@ func OpenNotes(openNotes []models.OpenNote) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"open-notes\" class=\"flex w-[calc(100%-200px)]\" hx-swap-oob=\"true\" hx-swap=\"outerHTML\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"open-notes\" class=\"flex items-end w-[calc(100%-200px)]\" hx-swap-oob=\"true\" hx-swap=\"outerHTML\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
